a45d54dea90ebf35ead11f787de8511f9c272133
Apington <thek1ng0ne@web.de>
2017-07-02 14:59:19 +0200

    Neue Variable, Besserer Lookup

--- OneCode/DAL/DataAcessor.cs
+++ OneCode/DAL/DataAcessor.cs
@@ -24,17 +24,17 @@ namespace DAL {
 
         public void FindVariablesInDoc(EnvDTE.TextDocument haystackDoc) {
             EnvDTE.EditPoint objEditPt = haystackDoc.StartPoint.CreateEditPoint();
-
             var tree = CSharpSyntaxTree.ParseText(objEditPt.GetText(haystackDoc.EndPoint));
             var Mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
             var compilation = CSharpCompilation.Create("MyCompilation", syntaxTrees: new[] { tree }, references: new[] { Mscorlib });
             var model = compilation.GetSemanticModel(tree);
 
-            var vars = tree.GetRoot().DescendantNodes().Where(v => v is VariableDeclarationSyntax || v is ParameterSyntax || v is PropertyDeclarationSyntax);
+            var vars = tree.GetRoot().DescendantNodes().Where(v => v is VariableDeclarationSyntax || v is ParameterSyntax || v is PropertyDeclarationSyntax || v is LocalDeclarationStatementSyntax);
 
             var property = tree.GetRoot().DescendantNodes().OfType<PropertyDeclarationSyntax>();
             var parameter = tree.GetRoot().DescendantNodes().OfType<ParameterSyntax>();
             var field = tree.GetRoot().DescendantNodes().OfType<FieldDeclarationSyntax>();
+            var variableDeclarations = tree.GetRoot().DescendantNodes().OfType<LocalDeclarationStatementSyntax>();
 
             //Get symbols
             foreach (var p in property) {
@@ -57,8 +57,15 @@ namespace DAL {
                     string visibleType = f.Declaration.Type.ChildTokens().Where(v => v.IsKeyword()).First().Value.ToString();
                     string text = fieldSymbol.OriginalDefinition.ToString();
 
-                    varCollection.Add(new Variable(name, text, haystackDoc, type));
+                    //varCollection.Add(new Variable(name, text, haystackDoc, type));
                 }
+            }
+
+            foreach (var variableDeclaration in variableDeclarations)
+            {
+                var symbolInfo = model.GetSymbolInfo(variableDeclaration.Declaration.Type);
+                var typeSymbol = symbolInfo.Symbol; // the type symbol for the variable..
+                
             }
 
             Translator.TranslateStringArray(new string[] { "Hello", "my", "name", "is", "Tim" });
--- OneCode/Model/Variable.cs
+++ OneCode/Model/Variable.cs
@@ -4,36 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EnvDTE;
+using System.Globalization;
 
 namespace OneCode {
     [Serializable]
     class Variable {
+
+        public CultureInfo FromLanguage { get; set; }
+        public CultureInfo ToLanguage { get; set; }
+        public String Type { get; set; }
         public String Name { get; set; }
-        public TextDocument Document { get; set; }
-        public CodeRange TextRange { get; set; }
         public String Text { get; set; }
-        public Type Type { get; set; }
-        public int LineNumber { get; set; }
+        public String Translation { get; set; }
 
         public Variable() {
 
         }
 
-        public Variable(String _name, String _txt, int _lineNumber, TextDocument _txtDoc, CodeRange _txtRng, Type _type) {
-            Name = _name;
-            Text = _txt;
-            Document = _txtDoc;
-            TextRange = _txtRng;
-            Type = _type;
-            LineNumber = _lineNumber;
-        }
-
-        public Variable(String _name, String _txt, TextDocument _txtDoc, Type _type) {
-            Name = _name;
-            Text = _txt;
-            Document = _txtDoc;
-            Type = _type;
-            LineNumber = 0;
-        }
     }
 }
